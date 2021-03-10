9591b17dea85880e7ede56f9c7032e48828d53b8
Jefferson Cechinel <jclorets@hotmail.com>
2021-03-10 02:50:48 -0300

    fix uuid return when a record is saved

--- app/ModelsTmp/Category.php
+++ app/ModelsTmp/Category.php
@@ -6,13 +6,18 @@ use App\models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Genre extends Model
+class Category extends Model
 {
     use SoftDeletes, Uuid;
 
-    protected $fillable = ['name', 'is_active'];
+    protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = [
         'id' => 'string'
     ];
+
+    // necessary for uuid as primary key
+    public $incrementing = false;
+
 }
+
--- app/ModelsTmp/Genre.php
+++ app/ModelsTmp/Genre.php
@@ -1,19 +1,21 @@
 <?php
 
-namespace App\models;
+namespace App\Models;
 
 use App\models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends Model
+class Genre extends Model
 {
     use SoftDeletes, Uuid;
 
-    protected $fillable = ['name', 'description', 'is_active'];
+    protected $fillable = ['name', 'is_active'];
     protected $dates = ['deleted_at'];
     protected $casts = [
         'id' => 'string'
     ];
-}
 
+    // necessary for uuid as primary key
+    public $incrementing = false;
+}
